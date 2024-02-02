70426a472426195144d9a4094d345bfbbea43ee1
Dominique-Laurent Couturier <dlc48@cam.ac.uk>
2024-02-02 16:21:57 +0000

    update instructions

--- update-package.R
+++ update-package.R
@@ -8,8 +8,33 @@ rm(list=ls())
 # devtools::create("BATS")
 
 
-# 
+# RUN THIS AFTER AN UPDATE 
 devtools::document() 
+
+
+############################################
+# github installation:
+
+# PUSH and then use
+devtools::install_github("dlc48/dotfunctions", auth_token = "XXXX")
+# where "auth_token" is generated from github.com
+# -> dlc48 / settings / Developer settings / Personal access token / 
+#    Tokens (classic) / generate new token 
+
+
+############################################
+# local installation:
+devtools::build(binary=FALSE)
+
+# install
+    # within R
+    devtools::install()
+    # outside R
+    R CMD INSTALL dotfunctions_1.0.0.tar.gz
+
+
+
 
 # build: NO NEED IF INSTALLED FROM GITHUB
 # devtools::build(binary=FALSE)
+
