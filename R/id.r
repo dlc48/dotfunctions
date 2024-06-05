b92ef70c01b3fc5a587a961622aa3c5892f1260f
Dominique-Laurent Couturier <dlc48@cam.ac.uk>
2024-06-05 13:54:52 +0100

    transfer .ar

--- R/id.r
+++ R/id.r
@@ -6,9 +6,11 @@
 #' @returns a data frame with columns 'pos' (scalar vector going from 1 to the number of rows), 'id' (character vector ordered by numerical value whe the input is numerical) and possibly 'n' (numerical vector) when the input is numerical or the output of the function table().
 #' @export
 #' @examples
+#' \dontrun{
 #' .idf(table(rpois(10,3)),"X")
 #' print(id.X)
 #' print(n.X)
+#' }
 .idf = function(input,name){
     # warnings
     w1 = ifelse(class(input)=="table",
@@ -46,7 +48,41 @@
 #' @returns output is a string vector.
 #' @export
 #' @examples    
+#' \dontrun{    
 #' .ide()
+#' }    
 .ide = function(){
     c(apropos("^id[.]"),apropos("^n[.]"))
     }
+
+#' @name .ar
+#' @title id files based arrays
+#' @description creates an array with dim(ensions) and dimnames coming from id files, typically generated via .idf().
+#' @param id1 first id file
+#' @param id2 second id file
+#' @param id3 third id file
+#' @param id4 fourth id file
+#' @param id5 fifth id file
+#' @param value content of the array, NA by default  
+#' @returns an array (or a matrix when only 2 id files are provided)
+#' @export
+#' @examples     
+#' \dontrun{       
+#' .idf(table(rpois(10,3)),"X")
+#' .idf(LETTERS[1:6],"Y")   
+#' .ar(id.X,id.Y)    
+#' }   
+.ar = function(id1,id2,id3=NULL,id4=NULL,id5=NULL,value=NA){
+    listw = list(id1,id2,id3,id4,id5)
+    listw = listw[!sapply(listw,is.null)]
+    # minimal checks
+    if(length(listw)<2){.w("provide at least 2 id.XX files");stop()}
+    if(any(sapply(listw,function(x)length(unique(x$id)))!=
+       sapply(listw,nrow))){.w("invalid id.XX files");stop()}
+    # define array
+    array(value,dim=sapply(listw,nrow),
+          dimnames=lapply(listw,function(x)x$id))
+}
+
+
+
